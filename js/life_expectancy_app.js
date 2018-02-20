b245fb0ab7aaede2e07f70f40103d42bb6120bf1
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-02-20 13:44:55 +0300

    Create XMLHttpRequest object

--- js/life_expectancy_app.js
+++ js/life_expectancy_app.js
@@ -1,5 +1,13 @@
 
-$.getJSON('http://miroshkin.netlify.com/json/life-expectancy.json', function(data) {
-    //data is the JSON string
-    console.log(data);
-});
+// $.getJSON('http://miroshkin.netlify.com/json/life-expectancy.json', function(data) {
+//     //data is the JSON string
+//     console.log(data);
+// });
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechande = function () {
+  if (xhr.readystate === 4) {
+    console.log(JSON.parse(xhr.responseText));
+  }
+}
+xhr.open("GET", "http://miroshkin.netlify.com/json/life-expectancy.json");
+xhr.send();
