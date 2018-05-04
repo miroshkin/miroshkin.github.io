8afbb6e54d07343f4837b308de71b22731e5f575
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-05-04 18:16:24 +0300

    Add quiz example

--- js/life_expectancy_app.js
+++ js/life_expectancy_app.js
@@ -1,38 +1,195 @@
+// // Added test comment
+//  $(document).ready(function () {
+//   $.getJSON('../../json/life-expectancy.json', function(response) {
+//       //data is the JSON string
+//
+//       var listHTML = "<ul>";
+//       $.each(response, function (index, data) {
+//         listHTML += "<li>";
+//         listHTML += data.Country + " " + data.BothSexesLifeExpectancy;
+//         listHTML += "</li>";
+//       });
+//       listHTML += "</ul>";
+//       $("#content").html(listHTML);
+//       console.log(listHTML);
+//     });
+//  });
 
-// $.getJSON('http://miroshkin.netlify.com/json/life-expectancy.json', function(data) {
-//     //data is the JSON string
-//     console.log(data);
-// });
-// var xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function () {
-//     if (xhr.readyState === 4) {
-//     var list = JSON.parse(xhr.responseText);
-//     var listHTML = "<ul>";
-//     for(var i=0; i<list.length; i += 1){
-//       listHTML += "<li>";
-//       listHTML += list[i].Country + " " + list[i].BothSexesLifeExpectancy;
-//       listHTML += "</li>";ы
-//     }
-//     listHTML += "</ul>";
-//     document.getElementById("content").innerHTML = listHTML;
-//   }
-// }
-// xhr.open("GET", "https://miroshkin.netlify.com/json/life-expectancy.json");
-// xhr.send();
+(function() {
+  var questions = [{
+    question: "How long are you going to live?",
+    choices: ['less','60 - 70 years', '70 - 80 years', '80 - 90 years', 'more']
 
-// Added test comment
- $(document).ready(function () {
-  $.getJSON('../../json/life-expectancy.json', function(response) {
-      //data is the JSON string
+  }, {
+    question: "What is your gender?",
+    choices: ['male', 'female', 'It does not matter']
+  }
+];
 
-      var listHTML = "<ul>";
-      $.each(response, function (index, data) {
-        listHTML += "<li>";
-        listHTML += data.Country + " " + data.BothSexesLifeExpectancy;
-        listHTML += "</li>";
-      });
-      listHTML += "</ul>";
-      $("#content").html(listHTML);
-      console.log(listHTML);
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#quiz'); //Quiz div object
+
+  // Display initial question
+  displayNext();
+
+  // Click handler for the 'next' button
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+
+    // Suspend click listener during fade animation
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+
+  // Click handler for the 'prev' button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+
+  // Click handler for the 'Start Over' button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+
+  // Creates and returns the div that contains the questions and
+  // the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
     });
- });
+
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+  function myMap() {
+      var mapOptions = {
+          center: new google.maps.LatLng(51.5, -0.12),
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.HYBRID
+      }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var country = "Russia";
+  var geocoder;
+  geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode( {'address' : country}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+      }
+  });
+
+  }
+
+
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+
+      if(questionCounter < questions.length){
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value='+selections[questionCounter]+']').prop('checked', true);
+        }
+
+        // Controls display of 'prev' button
+        if(questionCounter === 1){
+          $('#prev').show();
+        } else if(questionCounter === 0){
+
+          $('#prev').hide();
+          $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+        myMap();
+      }
+    });
+  }
+
+
+
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }
+
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right!!!');
+    return score;
+  }
+})();
